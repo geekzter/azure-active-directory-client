63d9c79f474795ac3827041adce0d5ae9dfa926b
Eric van Wijk <eric@van-wijk.com>
2023-02-25 10:19:39 +0100

    Demo script

---
+++ scripts/demo.ps1
@@ -0,0 +1,98 @@
+#!/usr/bin/env pwsh
+param ( 
+    [parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Workspace=($env:TF_WORKSPACE ?? 'default')
+) 
+
+function Prompt-User (
+    [parameter(Mandatory=$false)][string]
+    $PromptMessage = "Continue with next step",
+    [parameter(Mandatory=$false)][string]
+    $ContinueMessage = "Continue with next step",
+    [parameter(Mandatory=$false)][string]
+    $AbortMessage = "Aborting demo"
+) {
+    $defaultChoice = 0
+    # Prompt to continue
+    $choices = @(
+        [System.Management.Automation.Host.ChoiceDescription]::new("&Continue", $ContinueMessage)
+        [System.Management.Automation.Host.ChoiceDescription]::new("&Exit", $AbortMessage)
+    )
+    $decision = $Host.UI.PromptForChoice("`n", $PromptMessage, $choices, $defaultChoice)
+    Write-Debug "Decision: $decision"
+
+    if ($decision -eq 0) {
+        Write-Host "$($choices[$decision].HelpMessage)"
+    } else {
+        Write-Host "$($PSStyle.Formatting.Warning)$($choices[$decision].HelpMessage)$($PSStyle.Reset)"
+        exit $decision             
+    }
+}
+
+try {
+    Push-Location -Path $PSScriptRoot
+
+    # Step 1: Create application with Terraform
+    Prompt-User -PromptMessage "Create application with Terraform?" `
+                -ContinueMessage "Creating application with Terraform"
+    Write-Host "Running $(Resolve-Path ./deploy.ps1).Path"
+    ./deploy.ps1
+    Write-Host "`nEnterprise Application (Service Principal) url:"
+    terraform -chdir='../terraform' output application_portal_url
+    Write-Host "`nApplication registration url:"
+    terraform -chdir='../terraform' output application_registration_portal_url
+
+    # Step 1a: Propagate Terraform output to environment variables
+    Join-Path (Split-Path $PSScriptRoot -Parent) data $Workspace set_environment_variables.ps1 | Set-Variable envVarsScript
+    if (Test-Path $envVarsScript) {
+        . $envVarsScript
+    } else {
+        Write-Warning "No environment variables script found for Terraform workspace '${Workspace}'. Pleas run deploy.ps1 to create resources."
+        exit 1
+    }
+    . $envVarsScript
+    Write-Host "`nPropagating Terraform output as environment variables:"
+    Get-ChildItem -Path Env: -Recurse -Include AZURE_*NT_ID | Sort-Object -Property Name |  Format-Table -HideTableHeaders
+
+    # Step 2: Login with AAD
+    Prompt-User -PromptMessage "Log into AAD (opens browser window)?" `
+                -ContinueMessage "Opening browser window to log into AAD..."
+    Write-Host "Running $(Resolve-Path ./login.ps1)"
+    ./login.ps1
+
+    # Step 3: Capture redirected url
+    Write-Host "Copy the url the browser redirects to (http://localhost/?code=...&state=...)"
+    Read-Host -Prompt "Paste url here" | Set-Variable redirectUrl
+    Write-Host "`nPasted url:`n $redirectUrl"
+
+    # Step 4: Get token
+    ./login.ps1 -RedirectUrl $redirectUrl | Set-Variable accessToken
+    if ($accessToken) {
+        Write-Host "`nAccess token:`n $accessToken"            
+    } else {
+        Write-Error "Could not get access token from login.ps1"
+        exit 1
+    }
+} finally {
+    Pop-Location
+}
+
+# Step 5: Use token to install agent locally
+if (Test-Path ../../azure-pipeline-scripts/scripts/install_agent.ps1) {
+    try {
+        Push-Location -Path ../../azure-pipeline-scripts/scripts
+
+        Prompt-User -PromptMessage "Install Azure Pipeline agent locally?" `
+                    -ContinueMessage "Installing Azure Pipeline agent locally..."
+
+        Write-Host "Running $(Resolve-Path ./install_agent.ps1)"
+        ./install_agent.ps1 -Token $accessToken
+    } finally {
+        Pop-Location
+    }
+} else {
+    Write-Warning "Could not find ../../azure-pipeline-scripts/scripts/install_agent.ps1 script. Please clone https://github.com/geekzter/azure-pipeline-scripts into ../../azure-pipeline-scripts"
+    exit 1
+}
---
+++ scripts/login.ps1
@@ -0,0 +1,114 @@
+#!/usr/bin/env pwsh
+#Requires -Version 7
+[CmdletBinding(DefaultParameterSetName = 'GetToken')]
+param ( 
+    [parameter(Mandatory=$false,ParameterSetName="GetToken")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Code,
+
+    [parameter(Mandatory=$false,ParameterSetName="GetToken")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $State,
+
+    [parameter(Mandatory=$false,ParameterSetName="GetTokenWithRedirectUrl",ValueFromPipeline=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $RedirectUrl,
+
+    [parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Workspace=($env:TF_WORKSPACE ?? 'default')
+) 
+
+function Build-LoginUrl () {
+    $State ??= [guid]::NewGuid().Guid
+    $loginUrl = "https://login.microsoftonline.com/${env:AZURE_TENANT_ID}/oauth2/v2.0/authorize"
+    $loginUrl += "?client_id=${env:AZURE_CLIENT_ID}"
+    $loginUrl += "&scope=499b84ac-1321-427f-aa17-267ca6975798%2F.default"
+    $loginUrl += "&state=${State}"
+    $loginUrl += "&response_type=code"
+    $loginUrl += "&redirect_uri=$([uri]::EscapeDataString('http://localhost'))"
+    $loginUrl += "&response_mode=query"
+
+    Write-Debug "Login URL: ${loginUrl}"
+    return $loginUrl
+}
+
+function Build-TokenRequestBody (
+    [parameter(Mandatory=$true,ParameterSetName="Inline")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Code,
+
+    [parameter(Mandatory=$true,ParameterSetName="Inline")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $State,
+
+    [parameter(Mandatory=$true,ParameterSetName="Url")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $RedirectUrl
+) {
+    if ($RedirectUrl) {
+        $RedirectUrl -match "http(s)?://[^\/]+/\?code=(?<code>[^\&]+)\&state=(?<state>[^\&]+)"
+        $Matches | Out-String | Write-Debug
+        $Code = $Matches['code']
+        $State = $Matches['state']
+    }
+    if (!$Code) {
+        throw "Code is required"
+    }
+    if (!$State) {
+        throw "State is required"
+    }
+    Write-Debug "Code: ${Code}"
+    Write-Debug "State: ${State}"
+
+    $requestBody = "&scope=499b84ac-1321-427f-aa17-267ca6975798%2F.default"
+    $requestBody += "&code=${Code}"
+    $requestBody += "&redirect_uri=http%3A%2F%2Flocalhost"
+    $requestBody += "&grant_type=authorization_code"
+    $requestBody += "&state=${State}"
+    $requestBody += "&client_id=${env:AZURE_CLIENT_ID}"
+    Write-Debug "requestBody: ${requestBody}"
+    
+    return $requestBody
+}
+
+. (Join-Path $PSScriptRoot functions.ps1)
+
+# Propagate Terraform context
+Join-Path (Split-Path $PSScriptRoot -Parent) data $Workspace set_environment_variables.ps1 | Set-Variable envVarsScript
+if (Test-Path $envVarsScript) {
+    . $envVarsScript
+} else {
+    Write-Warning "No environment variables script found for Terraform workspace '${Workspace}'. Pleas run deploy.ps1 to create resources."
+    exit 1
+}
+. $envVarsScript
+
+if (!$Code -and !$RedirectUrl) {
+    $loginUrl = Build-LoginUrl
+    # Write-Host $logonUrl
+    Open-Browser -Url $loginUrl 
+} else {
+    if ($RedirectUrl) {
+        $requestBody = Build-TokenRequestBody -RedirectUrl $RedirectUrl
+    } elseif ($Code) {
+        $requestBody = Build-TokenRequestBody -Code $Code -State $State
+    }
+    $requestBody | Format-Table | Out-String | Write-Debug
+    Invoke-RestMethod -Uri "https://login.microsoftonline.com/${env:ARM_TENANT_ID}/oauth2/v2.0/token" `
+                      -Method Post `
+                      -ContentType 'application/x-www-form-urlencoded' `
+                      -Body $requestBody `
+                      | Set-Variable tokenResponse
+
+    $accessToken = $tokenResponse.access_token
+    Write-Debug "accessToken: ${accessToken}"
+    Write-Output $accessToken
+}
